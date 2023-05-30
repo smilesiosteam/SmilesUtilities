5bf1e0da8936c590cc321a9d28e46ace96c31e8f
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-05-30 13:12:09 +0500

    - Added shared model for AppUserInfo

---
+++ Sources/SmilesUtilities/SharedModels/AppUserInfo.swift
@@ -0,0 +1,57 @@
+//
+//  File.swift
+//  
+//
+//  Created by Abdul Rehman Amjad on 30/05/2023.
+//
+
+import Foundation
+
+public class AppUserInfo: Codable {
+    
+    public var isLocationUpdated: Bool?
+    public var latitude: String?
+    public var location: String?
+    public var locationId: String?
+    public var longitude: String?
+    public var mambaId: String?
+    public var cityId : Int?
+    public var nickName : String?
+    public var cityName: String?
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case isLocationUpdated
+        case latitude
+        case location
+        case locationId
+        case longitude
+        case mambaId
+        case cityId
+        case nickName
+        case cityName
+    }
+    
+    public init() {}
+    
+    required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        isLocationUpdated = try values.decodeIfPresent(Bool.self, forKey: .isLocationUpdated)
+        latitude = try values.decodeIfPresent(String.self, forKey: .latitude)
+        location = try values.decodeIfPresent(String.self, forKey: .location)
+        locationId = try values.decodeIfPresent(String.self, forKey: .locationId)
+        longitude = try values.decodeIfPresent(String.self, forKey: .longitude)
+        mambaId = try values.decodeIfPresent(String.self, forKey: .mambaId)
+        cityId =  try values.decodeIfPresent(Int.self, forKey: .cityId)
+        nickName = try values.decodeIfPresent(String.self, forKey: .nickName)
+        cityName = try values.decodeIfPresent(String.self, forKey: .cityName)
+    }
+    
+    public func asDictionary() -> [String: Any] {
+        let encoder = DictionaryEncoder()
+        guard let encoded = try? encoder.encode(self) as [String: Any] else {
+            return [:]
+        }
+        return encoded
+    }
+}
