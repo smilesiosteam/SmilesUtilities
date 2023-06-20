8ad10eeaaf1aa53dfbd6aea2b9998b39fe197fab
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-06-20 10:40:41 +0500

    - Added custom button and view

---
+++ Sources/SmilesUtilities/CustomViews/UICustomButton.swift
@@ -0,0 +1,114 @@
+//
+//  File.swift
+//  
+//
+//  Created by Abdul Rehman Amjad on 20/06/2023.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+public class UICustomButton: UIButton {
+    
+    public override class var layerClass: AnyClass {
+        CAGradientLayer.self
+    }
+    
+    private var gradientLayer: CAGradientLayer {
+        layer as! CAGradientLayer
+    }
+    
+    // button corner radius
+    @IBInspectable public var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    // button border color
+    @IBInspectable public var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    // button border width
+    @IBInspectable public var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    // button shadow color
+    @IBInspectable public var shadowColor: UIColor = UIColor.clear {
+        didSet{
+            self.layer.shadowColor = shadowColor.cgColor
+            self.layer.masksToBounds = false
+        }
+    }
+    
+    // button shadow radius
+    @IBInspectable public var shadowRadius: CGFloat = CGFloat(0.5) {
+        didSet {
+            self.layer.masksToBounds = false
+            self.layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    // button shadow opacity
+    @IBInspectable public var shadowOpacity: Float = 0.4 {
+        didSet {
+            self.layer.masksToBounds = false
+            self.layer.shadowOpacity = shadowOpacity
+        }
+    }
+    
+    // button shadow offset
+    @IBInspectable public var shadowOffset: CGSize = CGSize(width: 1, height: 4) {
+        didSet {
+            self.layer.masksToBounds = false
+            self.layer.shadowOffset = shadowOffset
+        }
+    }
+    
+    // button start color gradient
+    @IBInspectable public var startColor: UIColor = .white {
+        didSet {
+            updateColors()
+        }
+    }
+    
+    // button end color gradient
+    @IBInspectable public var endColor: UIColor = .red {
+        didSet {
+            updateColors()
+        }
+    }
+    
+    // button start point gradient
+    @IBInspectable public var startPoint: CGPoint {
+        get {
+            gradientLayer.startPoint
+        }
+        
+        set {
+            gradientLayer.startPoint = newValue
+        }
+    }
+    
+    // button end point gradient
+    @IBInspectable public var endPoint: CGPoint {
+        get {
+            gradientLayer.endPoint
+        }
+        set {
+            gradientLayer.endPoint = newValue
+        }
+    }
+    
+    // update colores gradient
+    private func updateColors() {
+        gradientLayer.colors = [startColor.cgColor, endColor.cgColor]
+    }
+}
