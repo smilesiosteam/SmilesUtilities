10d7ffb7a74a96609936ac875923b5a251174590
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-06-26 17:58:39 +0500

    - Added cell registrable protocol

---
+++ Sources/SmilesUtilities/Protocols/File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Abdul Rehman Amjad on 26/06/2023.
+//
+
+import Foundation
+import UIKit
+
+public protocol CellRegisterable {
+    func register(for tableView: UITableView)
+}
