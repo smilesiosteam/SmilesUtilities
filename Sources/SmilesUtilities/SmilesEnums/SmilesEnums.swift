77baa3c02b8ef37895d8f244115f18502d9313f9
Shahroze Zaheer <mshehroze@etisalat.ae>
2023-07-27 19:47:48 +0500

    Moved Offer types to utilities

---
+++ Sources/SmilesUtilities/SmilesEnums/SmilesEnums.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Shahroze Zaheer on 27/07/2023.
+//
+
+import Foundation
+
+public enum OfferTypes : String {
+    case discount = "discount", voucher = "voucher", dealVoucher = "deal voucher", etisalat = "etisalat", NoType = "-1"
+}
