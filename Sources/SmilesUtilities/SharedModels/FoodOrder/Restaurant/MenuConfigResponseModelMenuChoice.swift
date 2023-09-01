508739f5cecd3691011a301705d9fdfe9011666e
Hanan Ahmed <ahanan@etisalat.ae>
2023-09-01 19:01:14 +0400

    -- Menu subchoiceType List added

--- Sources/SmilesUtilities/SharedModels/FoodOrder/Restaurant/MenuConfigResponseModelMenuChoice.swift
+++ Sources/SmilesUtilities/SharedModels/FoodOrder/Restaurant/MenuConfigResponseModelMenuChoice.swift
@@ -15,6 +15,7 @@ public struct MenuConfigResponseModelMenuChoice : Codable {
     public let choiceType : String?
     public let choiceText : String?
     public let subChoices: [MenuConfigResponseModelMenuSubChoices]?
+    public let subChoiceTypeList: [MenuConfigResponseModelMenuChoice]?
     public let isMultipleChoiceAllowed: Bool?
     public let isChoiceMandatory: Bool?
     public let multipleChoice: Int?
@@ -35,6 +36,7 @@ public struct MenuConfigResponseModelMenuChoice : Codable {
         case minimumChoice
         case defaultSelection
         case quantityFlag
+        case subChoiceTypeList
     }
     
     public init(from decoder: Decoder) throws {
@@ -51,6 +53,7 @@ public struct MenuConfigResponseModelMenuChoice : Codable {
         minimumChoice = try values.decodeIfPresent(Int.self, forKey: .minimumChoice)
         defaultSelection = try values.decodeIfPresent(Int.self, forKey: .defaultSelection)
         quantityFlag = try values.decodeIfPresent(Bool.self, forKey: .quantityFlag)
+        subChoiceTypeList = try values.decodeIfPresent([MenuConfigResponseModelMenuChoice].self, forKey: .subChoiceTypeList)
     }
     
 }
