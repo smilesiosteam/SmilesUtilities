c28902009acfb5458900d5c6e7475a556bee6c14
Habib Rehman <haburrehman@eand.com>
2023-09-13 19:20:01 +0500

    moved Extension to the smileUtilities

--- Sources/SmilesUtilities/Extensions/UserDefaultExtensions/UserDefaultExtension.swift
+++ Sources/SmilesUtilities/Extensions/UserDefaultExtensions/UserDefaultExtension.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+public extension UserDefaults {
+    public func object<T: Codable>(_ type: T.Type, with key: String, usingDecoder decoder: JSONDecoder = JSONDecoder()) -> T? {
+        guard let data = self.value(forKey: key) as? Data else { return nil }
+        return try? decoder.decode(type.self, from: data)
+    }
+
+    public func set<T: Codable>(object: T, forKey key: String, usingEncoder encoder: JSONEncoder = JSONEncoder()) {
+        let data = try? encoder.encode(object)
+        self.set(data, forKey: key)
+    }
+}
