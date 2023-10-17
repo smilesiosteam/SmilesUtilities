a5b95e81c0c3fa9da275e4de762e6bc91af1cc10
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-10-17 12:56:14 +0500

    - Added SmilesSectionData enum

---
+++ Sources/SmilesUtilities/Enums/SmilesSectionData.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Abdul Rehman Amjad on 17/10/2023.
+//
+
+import Foundation
+
+public protocol SectionIdentifierProtocol {
+    var identifier: String { get }
+}
+
+public struct SmilesSectionData<T: SectionIdentifierProtocol> {
+    
+    public let index: Int
+    public let identifier: T
+    
+}
