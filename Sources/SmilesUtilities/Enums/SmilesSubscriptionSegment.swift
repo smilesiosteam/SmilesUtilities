5fe6444f7d6dc07a614a8529a2bc363fe66fa81c
SmilesDev <smilesiosteam@gmail.com>
2023-10-27 11:33:42 +0500

    feat: added SmilesSubscriptionSegment Enum

---
+++ Sources/SmilesUtilities/Enums/SmilesSubscriptionSegment.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ghullam  Abbas on 27/10/2023.
+//
+
+import Foundation
+
+public enum SmilesSubscriptionSegment: String {
+    case lifestyle = "lifestyle"
+    case smilesExplorer = "smilesExplorer"
+    case mcfc = "mcfc"
+}
