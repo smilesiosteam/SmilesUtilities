75c2baf56981eda5cb5a504e7c478cebe87ab9af
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-12-14 20:08:44 +0500

    - Fixed primary/secondary button issue on SmilesErrorView

--- Sources/SmilesUtilities/CustomViews/SmilesErrorView/View/SmilesErrorView.swift
+++ Sources/SmilesUtilities/CustomViews/SmilesErrorView/View/SmilesErrorView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SmilesFontsManager
 
 public enum ErrorViewType {
     case fullScreen, popUp
@@ -17,8 +18,6 @@ class SmilesErrorView: UIViewController {
     @IBOutlet weak var errorImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var primaryButton: UICustomButton!
-    @IBOutlet weak var secondaryButton: UICustomButton!
     @IBOutlet weak var popUpButtonsStack: UIStackView!
     @IBOutlet weak var buttonsStack: UIStackView!
     
@@ -27,13 +26,39 @@ class SmilesErrorView: UIViewController {
     private var error: SmilesError
     private weak var delegate: SmilesErrorViewDelegate?
     
+    // MARK: - VIEWS -
+    private let primaryButton: UICustomButton = {
+        let button = UICustomButton()
+        button.backgroundColor = UIColor(hexString: "75428E")
+        button.setTitleColor(.white, for: .normal)
+        button.fontTextStyle = .smilesHeadline4
+        button.cornerRadius = 24
+        button.clipsToBounds = true
+        button.addTarget(SmilesErrorView.self, action: #selector(primaryButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    private let secondaryButton: UICustomButton = {
+        let button = UICustomButton()
+        button.backgroundColor = .white
+        button.setTitleColor(UIColor(hexString: "75428E"), for: .normal)
+        button.borderWidth = 1.5
+        button.borderColor = UIColor(hexString: "75428E").withAlphaComponent(0.4)
+        button.fontTextStyle = .smilesHeadline4
+        button.cornerRadius = 24
+        button.clipsToBounds = true
+        button.addTarget(SmilesErrorView.self, action: #selector(secondaryButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     // MARK: - ACTIONS -
-    @IBAction func primaryButtonTapped(_ sender: Any) {
+    @objc func primaryButtonTapped() {
         dismiss(animated: true)
         delegate?.primaryButtonPressed()
     }
     
-    @IBAction func secondaryButtonTapped(_ sender: Any) {
+    @objc func secondaryButtonTapped() {
         dismiss(animated: true)
         delegate?.secondaryButtonPressed()
     }
@@ -69,12 +94,23 @@ class SmilesErrorView: UIViewController {
         
         primaryButton.setTitle(error.primaryButtonTitle, for: .normal)
         secondaryButton.setTitle(error.secondaryButtonTitle, for: .normal)
-        secondaryButton.isHidden = error.secondaryButtonTitle == nil
         
         self.errorViewType = error.errorViewType
         view.backgroundColor = errorViewType == .fullScreen ? .white : .black.withAlphaComponent(0.6)
         buttonsStack.isHidden = errorViewType == .popUp
         popUpButtonsStack.isHidden = errorViewType == .fullScreen
+        
+        if errorViewType == .popUp {
+            if error.secondaryButtonTitle != nil {
+                popUpButtonsStack.addArrangedSubview(secondaryButton)
+            }
+            popUpButtonsStack.addArrangedSubview(primaryButton)
+        } else {
+            if error.secondaryButtonTitle != nil {
+                buttonsStack.addArrangedSubview(secondaryButton)
+            }
+            buttonsStack.addArrangedSubview(primaryButton)
+        }
         
     }
     
