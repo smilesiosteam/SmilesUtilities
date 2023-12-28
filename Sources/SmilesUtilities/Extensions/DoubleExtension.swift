79ca318953f340e9af36c669a29df8b7a161218f
Muhammad Shayan Zahid <shahyan.zahid@systemsltd.com>
2023-12-28 15:34:57 +0500

    fix: double extension made public

--- Sources/SmilesUtilities/Extensions/DoubleExtension.swift
+++ Sources/SmilesUtilities/Extensions/DoubleExtension.swift
@@ -7,11 +7,11 @@
 
 import Foundation
 
-extension FloatingPoint {
+public extension FloatingPoint {
   var isInteger: Bool { rounded() == self }
 }
 
-extension Double {
+public extension Double {
     /// Rounds the double to decimal places value
     func rounded(toPlaces places: Int) -> Double {
         let divisor = pow(10.0, Double(places))
